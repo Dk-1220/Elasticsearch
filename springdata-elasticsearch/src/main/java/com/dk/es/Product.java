1988e78ecfe2f59f4b6e3dc30da126014dd03d3f
D <1285434409@qq.com>
2022-04-14 20:21:07 +0800

    StringData框架代码集成

---
+++ springdata-elasticsearch/src/main/java/com/dk/es/Product.java
@@ -0,0 +1,28 @@
+package com.dk.es;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Document(indexName = "product", shards = 3, replicas = 1)  // indexName：索引名称，shards：分片个数，replicas：备份个数
+public class Product {
+    @Id
+    private Long id;
+    @Field(type = FieldType.Text)
+    private String title;
+    @Field(type = FieldType.Keyword)
+    private String category;
+    @Field(type = FieldType.Double)
+    private Double price;
+    @Field(type = FieldType.Keyword, index = false)
+    private String images;
+}
---
+++ springdata-elasticsearch/src/main/java/com/dk/es/SpringdataElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.dk.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataElasticsearchApplication.class, args);
+    }
+
+}
